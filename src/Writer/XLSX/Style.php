74ae066e9b88d84fd4350b7447e8536fb61a649e
huangdongcheng <934313141@qq.com>
2020-02-18 22:23:23 +0800

    refactor

--- src/Writer/XLSX/Style.php
+++ src/Writer/XLSX/Style.php
@@ -14,6 +14,21 @@ class Style
 
     public $name;
 
+    protected $availableFormatCode = [
+        'General',
+        '0',
+        '0.00',
+        '#,##0',
+        '#,##0.00',
+        '@',
+        'yyyy-mm-dd',
+        'yyyy-mm-dd hh:mm:ss',
+        'hh:mm AM/PM',
+        'hh:mm:ss AM/PM',
+        'hh:mm',
+        'hh:mm:ss',
+    ];
+
     public function __construct(Workbook $workbook)
     {
         $this->name = 'styles.xml';
@@ -30,5 +45,25 @@ class Style
     public function getStyleXML()
     {
 
+    }
+
+    public function getFormatCode($code)
+    {
+        $formatCodeMap = [
+            'string'  => '@',
+            'integer' => '0',
+            'price'   => '#,##0.00',
+            'date'    => 'yyyy-mm-dd'
+        ];
+
+        if (isset($formatCodeMap[$code])) {
+            return $formatCodeMap[$code];
+        }
+
+        if (in_array($code, $this->availableFormatCode)) {
+            return $code;
+        }
+
+        return 'General';
     }
 }
