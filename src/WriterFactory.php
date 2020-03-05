706e8c807f3768f04968bf930193ce6e7ff01570
huangdongcheng <934313141@qq.com>
2020-03-05 22:40:18 +0800

    add comment for writer file

--- src/WriterFactory.php
+++ src/WriterFactory.php
@@ -7,11 +7,19 @@ use Rocky114\Excel\Writer\CSV\Writer as CSVWriter;
 
 class WriterFactory
 {
+    /**
+     * @param array $config
+     * @return \Rocky114\Excel\Writer\CSV\Writer
+     */
     public static function createCSVWriter(array $config)
     {
         return new CSVWriter($config);
     }
 
+    /**
+     * @param array $config
+     * @return \Rocky114\Excel\Writer\XLSX\Writer
+     */
     public static function createXLSXWriter(array $config = [])
     {
         return new XLSXWriter($config);
